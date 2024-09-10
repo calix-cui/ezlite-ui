33987e8001f2711481164fc27d040a61037a7c18
cuixiang <cuixiang405@gmail.com>
2024-09-10 16:20:18 +0800

    fix: 生产环境路径错误

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   plugins: [
     vue(),
     vueJsx(),
