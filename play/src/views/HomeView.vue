01288df78f614a7a40581e41416d0ed856cef4ce
cuixiang <cuixiang405@gmail.com>
2024-09-11 14:23:53 +0800

    feat：增加主页信息

--- play/src/views/HomeView.vue
+++ play/src/views/HomeView.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    HomeView
+  <div class="pl-8">
+    <div class="mb-4">
+      <h1 class="text-2xl">Welcome to EZLite</h1>
+      <p>A simple, minimalist, and easy-to-use Vue 3 component library.</p>
+    </div>
+
+    <section class="intro">
+      <h2 class="text-2xl">What is EZLite? </h2>
+      <p>EZLite is a lightweight Vue 3 component library designed to make it easy to build modern, fast, and elegant web
+        applications.</p>
+      <ul>
+        <li class="mb-2">📦 Minimal setup</li>
+        <li class="mb-2">🚀 Lightweight and fast</li>
+        <li class="mb-2">💡 Easy-to-use components</li>
+        <li class="mb-2">🎨 Customizable with minimal effort</li>
+        <li>🛠️ Built for Vue 3</li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script setup>
-
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
