77010d45af1c228aa468455d3ee8d7c4ebf8ce1f
cuixiang <cuixiang405@gmail.com>
2024-09-19 15:54:41 +0800

    feat: 开发 button 组件

--- packages/components/src/Button/Button.vue
+++ packages/components/src/Button/Button.vue
@@ -1,53 +1,99 @@
 <template>
-  <button class="ezlite-btn"
-    :class="['ezlite-btn-' + type, { 'ezlite-btn-dangerous': danger, 'ezlite-btn-ghost': ghost }]">
-    {{ icon }}
-    <slot name="icon" v-if="!icon" />
-    <slot />
+  <button
+    v-if="href === undefined"
+    class="ezlite-btn"
+    :class="[
+      'ezlite-btn-' + type,
+      'ezlite-btn-shape-' + shape,
+      'ezlite-btn-size-' + size,
+      {
+        'ezlite-btn-dangerous': danger,
+        'ezlite-btn-ghost': ghost,
+        // 'ezlite-btn-icon-only':
+      },
+    ]"
+    :disabled="disabled"
+    @click="handleClick"
+  >
+    <template v-if="icon">
+      <component :is="icon"></component>
+    </template>
+    <slot name="icon" v-else />
+
+    <span>
+      <slot />
+    </span>
   </button>
+  <a
+    v-else
+    class="ezlite-btn"
+    :class="[
+      'ezlite-btn-' + type,
+      'ezlite-btn-size-' + size,
+      {
+        'ezlite-btn-dangerous': danger,
+        'ezlite-btn-ghost': ghost,
+      },
+    ]"
+    :disabled="disabled"
+    :href="href"
+    :target="target"
+    @click="handleClick"
+  >
+    <slot />
+  </a>
 </template>
 
 <script setup lang="ts">
-import { PropType } from 'vue';
+import { PropType } from 'vue'
 
 defineOptions({
-  name: 'EButton'
+  name: 'EButton',
 })
 
+const emit = defineEmits(['click'])
+
 type ButtonType = 'default' | 'primary' | 'dashed' | 'link' | 'text'
-type ButtonShape = 'default' | 'circle' | 'round';
-type Size = 'large' | 'middle' | 'small';
+type ButtonShape = 'default' | 'circle' | 'round'
+type Size = 'large' | 'middle' | 'small'
 
 // vue3.5 之后，响应式 Props 解构，不需要再响应式处理
-const props = defineProps({
+const { type, disabled, icon } = defineProps({
   type: {
     type: String as PropType<ButtonType>,
-    default: 'default'
+    default: 'default',
   },
   danger: Boolean,
   disabled: Boolean,
   ghost: Boolean,
   href: String,
-  // boolean | { delay: number }
+  target: String,
   loading: {
     type: [Boolean, Object] as PropType<boolean | { delay: number }>,
-    default: false
+    default: false,
   },
   shape: {
     type: String as PropType<ButtonShape>,
-    default: 'default'
+    default: 'default',
   },
   size: {
     type: String as PropType<Size>,
-    default: 'middle'
+    default: 'middle',
   },
-  icon: null
+  icon: null,
 })
 
-
+const handleClick = (e: any) => {
+  if (disabled) {
+    e.preventDefault()
+    return
+  }
+  emit('click')
+}
 </script>
 
 <style lang="scss" scoped>
+// 各类型 danger 样式
 // .default.danger
 .ezlite-btn-default.ezlite-btn-dangerous:not(:disabled) {
   border-color: #ff4d4f;
@@ -76,7 +122,6 @@ const props = defineProps({
 .ezlite-btn-primary.ezlite-btn-dangerous:not(:disabled):active {
   background-color: #d9363e;
 }
-
 
 // .dashed.danger
 .ezlite-btn-dashed.ezlite-btn-dangerous:not(:disabled) {
@@ -122,6 +167,7 @@ const props = defineProps({
   color: #d9363e;
 }
 
+// 各类型 ghost 样式
 // .default.ghost
 .ezlite-btn-default.ezlite-btn-ghost {
   color: #fff;
@@ -175,10 +221,14 @@ const props = defineProps({
   border-color: #ff4d4f;
   box-shadow: none;
 }
+
 .ezlite-btn-default.ezlite-btn-dangerous.ezlite-btn-ghost:not(:disabled):hover {
   background-color: transparent;
 }
-.ezlite-btn-default.ezlite-btn-dangerous.ezlite-btn-ghost:not(:disabled):active {
+
+.ezlite-btn-default.ezlite-btn-dangerous.ezlite-btn-ghost:not(
+    :disabled
+  ):active {
   background-color: transparent;
 }
 
@@ -189,12 +239,16 @@ const props = defineProps({
   border-color: #ff4d4f;
   box-shadow: none;
 }
+
 .ezlite-btn-primary.ezlite-btn-dangerous.ezlite-btn-ghost:not(:disabled):hover {
   background-color: transparent;
   color: #ff7875;
   border-color: #ff7875;
 }
-.ezlite-btn-primary.ezlite-btn-dangerous.ezlite-btn-ghost:not(:disabled):active {
+
+.ezlite-btn-primary.ezlite-btn-dangerous.ezlite-btn-ghost:not(
+    :disabled
+  ):active {
   background-color: transparent;
   color: #d9363e;
   border-color: #d9363e;
@@ -207,18 +261,20 @@ const props = defineProps({
   border-color: #ff4d4f;
   box-shadow: none;
 }
+
 .ezlite-btn-dashed.ezlite-btn-dangerous.ezlite-btn-ghost:not(:disabled):hover {
   background-color: transparent;
   color: #ff7875;
   border-color: #ff7875;
 }
+
 .ezlite-btn-dashed.ezlite-btn-dangerous.ezlite-btn-ghost:not(:disabled):active {
   background-color: transparent;
   color: #d9363e;
   border-color: #d9363e;
 }
 
-// 基础样式
+// 按钮基础样式
 .ezlite-btn {
   font-size: 14px;
   height: 32px;
@@ -242,8 +298,33 @@ const props = defineProps({
   transition: outline-offset 0s, outline 0s;
 }
 
+// 形状 shape
+.ezlite-btn-shape-circle {
+  min-width: 32px;
+  padding-inline-start: 0;
+  padding-inline-end: 0;
+  border-radius: 50%;
+}
+.ezlite-btn-shape-round {
+  border-radius: 32px;
+}
 
-// default 
+// 尺寸 size
+.ezlite-btn-size-large {
+  font-size: 16px;
+  height: 40px;
+  padding: 6px 15px;
+  border-radius: 8px;
+}
+.ezlite-btn-size-small {
+  font-size: 14px;
+  height: 24px;
+  padding: 0px 7px;
+  border-radius: 4px;
+}
+
+// 各类型基础样式
+// default dashed
 .ezlite-btn-default,
 .ezlite-btn-dashed {
   background: #fff;
@@ -265,12 +346,13 @@ const props = defineProps({
 
 // primary
 .ezlite-btn-primary {
-  background-color: #1890ff;
+  background-color: #1677ff;
   color: #fff;
 }
 
 .ezlite-btn-primary:hover {
   background-color: #40a9ff;
+  color: #fff;
 }
 
 .ezlite-btn-primary:active {
@@ -309,5 +391,27 @@ const props = defineProps({
 
 .ezlite-btn-link:active {
   color: #0958d9;
+}
+
+// 不可用状态 disabled
+.ezlite-btn-default:disabled,
+.ezlite-btn-primary:disabled,
+.ezlite-btn-dashed:disabled {
+  cursor: not-allowed;
+  background-color: rgba(0, 0, 0, 0.04);
+  color: rgba(0, 0, 0, 0.25);
+  border-color: #d9d9d9;
+  box-shadow: none;
+}
+
+.ezlite-btn-text:disabled,
+.ezlite-btn-link:disabled,
+.ezlite-btn-link[disabled='true'] {
+  cursor: not-allowed;
+  color: rgba(0, 0, 0, 0.25);
+}
+
+.ezlite-btn-text:disabled:hover {
+  background-color: transparent;
 }
 </style>
