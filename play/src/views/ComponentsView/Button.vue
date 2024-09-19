cb61b8659e29c6c25061d17ab986c09323866b0d
cuixiang <cuixiang405@gmail.com>
2024-09-19 15:55:48 +0800

    docs: 更新文档

--- play/src/views/ComponentsView/Button.vue
+++ play/src/views/ComponentsView/Button.vue
@@ -29,42 +29,74 @@
 
     <a-divider></a-divider> -->
 
-    <a-divider orientation="left">按钮类型</a-divider>
+    <a-divider orientation="left">按钮类型 type</a-divider>
     <e-button class="mr-2">Default</e-button>
     <e-button class="mr-2" type="primary">Primary</e-button>
     <e-button class="mr-2" type="dashed">Dashed</e-button>
     <e-button class="mr-2" type="text">Text</e-button>
-    <e-button type="link">Link</e-button>
+    <e-button type="link" href="" target="_blank">Link</e-button>
 
-    <a-divider orientation="left">危险按钮</a-divider>
+    <a-divider orientation="left">危险按钮 danger</a-divider>
     <e-button class="mr-2" danger>Default</e-button>
     <e-button class="mr-2" danger type="primary">Primary</e-button>
     <e-button class="mr-2" danger type="dashed">Dashed</e-button>
     <e-button class="mr-2" danger type="text">Text</e-button>
-    <e-button class="mr-2" danger type="link">Link</e-button>
+    <e-button class="mr-2" danger type="link" href="" target="_blank"
+      >Link</e-button
+    >
 
-    <a-divider orientation="left">幽灵按钮</a-divider>
-    <div style="background-color: #c0c8c8;padding: 10px;">
+    <a-divider orientation="left">幽灵按钮 ghost</a-divider>
+    <div style="background-color: #c0c8c8; padding: 10px">
       <e-button class="mr-2" ghost>Default</e-button>
       <e-button class="mr-2" ghost type="primary">Primary</e-button>
       <e-button class="mr-2" ghost type="dashed">Dashed</e-button>
       <e-button class="mr-2" ghost danger>Danger</e-button>
     </div>
 
-    <!-- <div>danger ghost</div>
-    <div style="background-color: gray;padding: 3px;">
-      <e-button class="mr-2" danger ghost>Default</e-button>
-      <e-button class="mr-2" danger ghost type="primary">Primary</e-button>
-      <e-button class="mr-2" danger ghost type="dashed">Dashed</e-button>
-    </div> -->
+    <a-divider orientation="left">不可用状态 disabled</a-divider>
+    <e-button class="mr-2" disabled>Default(disabled)</e-button>
+    <e-button class="mr-2" type="primary" disabled>Primary(disabled)</e-button>
+    <e-button class="mr-2" type="dashed" disabled>Dashed(disabled)</e-button>
+    <e-button class="mr-2" type="text" disabled>Text(disabled)</e-button>
+    <e-button type="link" href="" target="_blank" disabled
+      >Link(disabled)</e-button
+    >
+
+    <a-divider orientation="left">按钮形状 shape</a-divider>
+    <e-button class="mr-2" type="primary">Default</e-button>
+    <e-button class="mr-2" type="primary" shape="circle">Circle</e-button>
+    <e-button class="mr-2" type="primary" shape="round">Round</e-button>
+
+    <a-divider orientation="left">按钮尺寸 size</a-divider>
+    <e-button class="mr-2" type="primary">Middle</e-button>
+    <e-button class="mr-2" type="primary" size="large">Large</e-button>
+    <e-button class="mr-2" type="primary" size="small">Small</e-button>
+
+    <a-divider orientation="left">icon</a-divider>
+    <e-button
+      class="mr-2"
+      type="primary"
+      shape="circle"
+      :icon="h(SearchOutlined)"
+    ></e-button>
+    <e-button class="mr-2" type="primary">
+      <template #icon>
+        <div class="flex items-center">
+          <component class="mr-1" :is="h(SearchOutlined)"></component>
+          Search
+        </div>
+      </template>
+    </e-button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { SearchOutlined } from '@ant-design/icons-vue';
-import { h, ref } from 'vue';
+import { SearchOutlined } from '@ant-design/icons-vue'
+import { h, ref } from 'vue'
 
-
+const onClick_btn = (type: string) => {
+  alert('clicked ' + type)
+}
 </script>
 
 <style lang="scss" scoped></style>
